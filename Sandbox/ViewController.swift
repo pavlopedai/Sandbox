78bdadaa8139e2dbe0be5823a35ddfc5a6a38424
Pavlo Pedai <pavlopedai@gmail.com>
2019-12-04 10:45:47 +0200

    Initial Commit

---
+++ Sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sandbox
+//
+//  Created by Pavlo Pedai on 4/12/19.
+//  Copyright © 2019 Pavlo Pedai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
